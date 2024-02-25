48b9c5c4f7b44989f77834b9d02cbba6a5c00c6d
Uzayr Iqbal <hamiduzayr@gmail.com>
2024-02-25 20:55:29 +0530

    updating project

---
+++ feed/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class PostForm(forms.Form):
+    image = forms.FileField()
+    text = forms.CharField(label="Description")
+    
--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,37 @@
 from typing import Any
-from django.views.generic import TemplateView
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from django.views.generic import TemplateView, DetailView, FormView
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
+
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
+        context['posts'] = Post.objects.all().order_by('-id')
         return context
+    
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+
+class AddPostView(FormView):
+    template_name = "new_post.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        new_object = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image']
+        )
+        messages.add_message(self.request, messages.SUCCESS, 'Your post was successful')
+        return super().form_valid(form)
