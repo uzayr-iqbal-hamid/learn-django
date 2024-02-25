c553d13b1d3d8b8754662906bfbc41a0dba44d8a
Uzayr Iqbal <hamiduzayr@gmail.com>
2024-02-25 15:06:58 +0530

    adding all files

---
+++ feed/views.py
@@ -0,0 +1,11 @@
+from typing import Any
+from django.views.generic import TemplateView
+from .models import Post
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        return context
